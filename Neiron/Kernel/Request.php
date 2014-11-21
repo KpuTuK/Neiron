1c8a9a1c0dd65e6b3baa5bc15bfc223b88750ed0
KpuTuK <KpuTuK@CodeMania-Team>
2014-11-22 01:28:10 +0400

    Реализованы методы globals; get, post, server (через globals)

--- Neiron/Kernel/Request.php
+++ Neiron/Kernel/Request.php
@@ -14,51 +14,57 @@ use Neiron\Arhitecture\Kernel\RequestInterface;
  * @author KpuTuK
  */
 class Request implements RequestInterface {
-    private $globals;
+    private $globals = array();
+    public function __construct() {
+        $this->globals($GLOBALS);
+    }
     public function create($uri = null, $method = self::METH_GET) {}
     /**
-     * @todo /
      * @param type $name
      * @param type $value
      * @return boolean
      */
-    public function globals($name = null, $value = null) {
+    public function globals($name = null, $value = null, $var = false) {
+        $glob = $var ? $this->globals[$var] : $this->globals;
         // Слияние массивов с заменой
         if (is_array($name)) {
-            $this->globals = array_merge($this->globals, $name);
+            return $this->globals = array_merge($glob, $name);
         }
         // Передача всего содержимого
         if ($name === null && $value === null) {
-            return $this->globals;
+            return $glob;
         }
         // Проверка на наличие и передача переменной из массива
         if ($name !== null && $value === null) {
-            if (array_key_exists($name, $this->globals)) {
-                return $this->globals[$name];
+            if (array_key_exists($name, $glob)) {
+                return $glob[$name];
             }
             return false;
         }
         // Запись переменной
         if ($name !== null && $value !== null) {
-            $this->globals[$name] = $value;
+           return $glob[$name] = $value;
         }
     }
     /**
-     * @todo ////
      * @param type $name
      * @param type $value
      */
-    public function server($name = null, $value = null) {}
+    public function server($name = null, $value = null) {
+        return $this->globals($name, $value, '_SERVER');
+    }
     /**
-     * @todo //
      * @param type $name
      * @param type $value
      */
-    public function get($name = null, $value = null) {}
+    public function get($name = null, $value = null) {
+        return $this->globals($name, $value, '_GET');
+    }
     /**
-     * @todo ///
      * @param type $name
      * @param type $value
      */
-    public function post($name = null, $value = null) {}
+    public function post($name = null, $value = null) {
+        return $this->globals($name, $value, '_POST');
+    }
 }
