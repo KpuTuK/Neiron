1d50c41ce8aef8dea8b7c445a62a771c0c5b93be
KpuTuK <KpuTuK@m.dcms.ru>
2014-11-26 00:24:30 +0400

    Добавлена документация. Добавлен метод run

--- Neiron/Arhitecture/Kernel/ApplicationInterface.php
+++ Neiron/Arhitecture/Kernel/ApplicationInterface.php
@@ -1,20 +1,47 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-namespace Neiron\Arhitecture\Kernel;
-
 /**
- *
- * @author KpuTuK
+ * PHP 5x framework с открытым иходным кодом
  */
-interface ApplicationInterface extends \ArrayAccess  {
+namespace Neiron\Arhitecture\Kernel;
+/**
+ * Интерфейс для реализации базового класса
+ * @author KpuTuK
+ * @version 1.0.0
+ * @package Neiron framework
+ * @category Kernel
+ * @link
+ */
+interface ApplicationInterface {
+    /**
+     * Добавляет обработчик роута по паттерну вызываемого методом GET
+     * @param sring $name Имя роута
+     * @param string $pattern Паттерн обработки роута
+     * @param mixed $handler Обработчик роута
+     */
     public function get($name, $pattern, $handler);
+    /**
+     * Добавляет обработчик роута по паттерну вызываемого методом POST
+     * @param sring $name Имя роута
+     * @param string $pattern Паттерн обработки роута
+     * @param mixed $handler Обработчик роута
+     */
     public function post($name, $pattern, $handler);
+    /**
+     * Добавляет обработчик роута по паттерну вызываемого методом PUT
+     * @param sring $name Имя роута
+     * @param string $pattern Паттерн обработки роута
+     * @param mixed $handler Обработчик роута
+     */
     public function put($name, $pattern, $handler);
+    /**
+     * Добавляет обработчик роута по паттерну вызываемого методом PUT
+     * @param sring $name Имя роута
+     * @param string $pattern Паттерн обработки роута
+     * @param mixed $handler Обработчик роута
+     */
     public function delete($name, $pattern, $handler);
+    /**
+     * Запускает приложение
+     */
+    public function run();
 }
