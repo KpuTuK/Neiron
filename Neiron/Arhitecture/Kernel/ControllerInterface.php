6b6f620974bd18b7f4ea0273f0e3011676fec327
KpuTuK <KpuTuK@CodeMania-Team>
2014-11-28 11:14:33 +0300

    Добавленна документация

--- Neiron/Arhitecture/Kernel/ControllerInterface.php
+++ Neiron/Arhitecture/Kernel/ControllerInterface.php
@@ -1,18 +1,23 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-namespace Neiron\Arhitecture\Kernel;
-
 /**
- *
+ * PHP 5x framework с открытым иходным кодом
+ */
+namespace Neiron\Arhitecture\Kernel;
+/**
+ * Главный контроллер
  * @author KpuTuK
+ * @version 1.0.0
+ * @package Neiron framework
+ * @category Kernel
+ * @link
  */
 interface ControllerInterface {
+    /**
+     * Функция вызываемая перед вызовом экшена контроллера
+     */
     public function atfer();
+    /**
+     * Функция вызываемая после вызовоа экшена контроллера
+     */
     public function beforle();
 }
