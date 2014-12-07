813d05c99dfffd6f85f688b98fded33cf324f747
KpuTuK <KpuTuK@CodeMania-Team>
2014-12-07 23:38:19 +0300

    ResponseHeaders - Удален метод headers() -  Поправлен код

--- Neiron/Kernel/Response/ResponseHeaders.php
+++ Neiron/Kernel/Response/ResponseHeaders.php
@@ -30,43 +30,10 @@ class ResponseHeaders implements ResponseHeadersInterface
      */
     public function __construct(array $headers, RequestInterface $request)
     {
-        $list = array();
-        foreach ($request->server as $key => $value) {
-            if (strpos($key, 'HTTP_') !== false) {
-                $list[
-                        ucfirst(substr(strtolower(strtr($key, '_', '-')), 5)) . ':'
-                        ] = $value;
-            }
-        }
-        $this->headers = array_replace($list, $headers);
-        $this->cookies = $request->cookies->getAll(true);
-    }
-    /**
-     * Удаляет/добавляет/выводит заголовки
-     * @param mixed $name Массив заголовков или имя заголовка
-     * @param string $value Содержимое заголовка
-     * @return mixed
-     */
-    public function headers($name = null, $value = null)
-    {
-        if (is_array($name)) {
-            return $this->headers = array_merge($this->headers, $name);
-        }
-        // Передача всего содержимого
-        if ($name === null && $value === null) {
-            return $this->headers;
-        }
-        // Проверка на наличие и передача переменной из массива
-        if ($name !== null && $value === null) {
-            if (array_key_exists($name, $this->headers)) {
-                return $this->headers[$name];
-            }
-            return false;
-        }
-        // Запись переменной
-        if ($name !== null && $value !== null) {
-            return $this->headers[$name] = $value;
-        }
+        
+        $this->headers = $request->headers;
+        $this->headers->merge($headers);
+        $this->cookies = $request->cookie->getFromHeader();
     }
     /**
      * Отпраляет заголовки если они еще не были отправлены
@@ -77,12 +44,18 @@ class ResponseHeaders implements ResponseHeadersInterface
         if (headers_sent()) {
             return $this;
         }
-        foreach ($this->headers as $key => $value) {
+        foreach ($this->headers->getAll() as $key => $value) {
             header($key . ' ' . $value);
         }
         foreach ($this->cookies as $cookie) {
             setcookie(
-                    $cookie['key'], $cookie['value'], $cookie['ttl'], $cookie['path'], $cookie['domain'], $cookie['secure'], $cookie['httponly']
+                $cookie['key'],
+                $cookie['value'],
+                $cookie['ttl'],
+                $cookie['path'],
+                $cookie['domain'],
+                $cookie['secure'],
+                $cookie['httponly']
             );
         }
         return $this;
