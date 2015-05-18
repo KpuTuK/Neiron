036268d2dba1833011ff9addad279c36760c4c1b
root <root@debian8>
2015-05-18 19:23:25 +0300

    Revert "Удален метод getAll() класса Neiron\Kernel\Helpers\ParameterManager"
    This reverts commit 60c50ed73be5bbbdcf830f353cbeb958805adca3.

--- Neiron/Kernel/Helpers/ParameterManager.php
+++ Neiron/Kernel/Helpers/ParameterManager.php
@@ -7,60 +7,15 @@ namespace Neiron\Kernel\Helpers;
 /**
  * Менеджер параметров
  * @author KpuTuK
- * @author Fabien Potencier <fabien@symfony.com>
  * @version 1.0.0
  * @package Neiron framework
  * @category Kernel
  * @link
  */
-class ParameterManager implements \ArrayAccess, \Countable, \IteratorAggregate
+class ParameterManager extends \ArrayObject
 {
-    protected $parameters = array();
-    public function __construct(array $parameters)
+    public function merge(array $params) 
     {
-            $this->parameters = $parameters;
-    }
-    public function merge(array $parameters)
-    {
-        $this->data = array_merge($this->parameters, $parameters);
-    }
-    public function getAll()
-    {
-        return $this->parameters;
-    }
-    public function getKeys()
-    {
-        return array_keys($this->parameters);
-    }
-    public function offsetExists($offset)
-    {
-        return array_key_exists($offset, $this->parameters);
-    }
-    public function offsetSet($offset, $value)
-    {
-        $this->parameters[$offset] = $value;
-    }
-    public function offsetGet($offset)
-    {
-        if ($this->offsetExists($offset)) {
-            return $this->parameters[$offset];
-        }
-        return false;
-    }
-    public function offsetUnset($offset)
-    {
-        if ($this->offsetExists($offset)) {
-            unset($this->parameters[$offset]);
-            return true;
-        }
-        return false;
-    }
-    public function count($mode = 'COUNT_NORMAL')
-    {
-        return count($this->parameters, $mode);
-    }
-    public function getIterator()
-    {
-        return new \ArrayIterator($this->parameters);
+        return $this->exchangeArray(array_merge((array)$this->getArrayCopy(), $params));
     }
 }
