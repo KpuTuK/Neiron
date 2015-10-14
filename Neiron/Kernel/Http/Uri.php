37b246012cf0b5e5c31913d5108214086e9adec8
KpuTuK <KpuTuK@CodeMania-Team>
2015-10-15 01:13:39 +0300

    [ADD] Добавлены @phpdoc комментарии

--- Neiron/Kernel/Http/Uri.php
+++ Neiron/Kernel/Http/Uri.php
@@ -15,6 +15,10 @@ namespace Neiron\Kernel\Http;
  *      согласно http://www.php-fig.org/psr/psr-7/
  */
 class Uri implements \Psr\Http\Message\UriInterface {
+    /**
+     * Массив данных uri
+     * @var array
+     */
     protected $parseUri = [
         'scheme' => '',
         'host' => '',
@@ -25,13 +29,24 @@ class Uri implements \Psr\Http\Message\UriInterface {
         'query' => '',
         'fragment' => '',
     ];
+    /**
+     * Конструктор класса
+     * @param string $uri Строка uri
+     */
     public function __construct($uri) {
-        array_replace($this->parseUri, parse_url($uri));
+        $this->parseUri = array_replace($this->parseUri, parse_url($uri));
     }
+    /**
+     * Выводит строку uri
+     * @return string Строка uri
+     */
     public function __toString() {
         return implode('', $this->parseUri);
     }
-
+    /**
+     * Формирует и возвращает данные авторизации
+     * @return string Данные авторизации
+     */
     public function getAuthority() {
         if ($this->parseUri['host'] === '') {
             return '';
@@ -40,65 +55,115 @@ class Uri implements \Psr\Http\Message\UriInterface {
             return $this->getUserInfo() .'@'. $this->parseUri['host'];
         }
     }
-
+    /**
+     * Возвращает фрагмент
+     * @return string Фрагмент
+     */
     public function getFragment() {
         return $this->parseUri['fragment'];
     }
-
+    /**
+     * Возвращает хост
+     * @return string
+     */
     public function getHost() {
         return $this->parseUri['host'];
     }
-
+    /**
+     * Возвращает путь
+     * @return string
+     */
     public function getPath() {
         return $this->parseUri['path'];
     }
-
+    /**
+     * Возвращает порт
+     * @return string
+     */
     public function getPort() {
         return $this->parseUri['port'];
     }
-
+    /**
+     * Возвращает данные запроса (после "?")
+     * @return string
+     */
     public function getQuery() {
         return $this->parseUri['query'];
     }
-
+    /**
+     * Возвращает схему запроса
+     * @return string
+     */
     public function getScheme() {
         return $this->parseUri['scheme'];
     }
-
+    /**
+     * Возвращает пароль и логин авторизации
+     * @return string
+     */
     public function getUserInfo() {
         return $this->parseUri['user'] .':'. $this->parseUri['pass'];
     }
-
+    /**
+     * Записывает фрагмент
+     * @param string $fragment
+     * @return \Neiron\Kernel\Http\Uri
+     */
     public function withFragment($fragment) {
         $this->parseUri['fragment'] = (string)$fragment;
         return $this;
     }
-
+    /**
+     * Записывает хост
+     * @param string $host
+     * @return \Neiron\Kernel\Http\Uri
+     */
     public function withHost($host) {
         $this->parseUri['host'] = (string)$host;
         return $this;
     }
-
+    /**
+     * Записывает путь
+     * @param string $path
+     * @return \Neiron\Kernel\Http\Uri
+     */
     public function withPath($path) {
         $this->parseUri['path'] = (string)$path;
         return $this;
     }
-
+    /**
+     * Записывает порт
+     * @param string $port
+     * @return \Neiron\Kernel\Http\Uri
+     */
     public function withPort($port) {
         $this->parseUri['port'] = (int)$port;
         return $this;
     }
-
+    /**
+     * Записывает данные запроса (после "?")
+     * @param string $query
+     * @return \Neiron\Kernel\Http\Uri
+     */
     public function withQuery($query) {
         $this->parseUri['query'] = (string)$query;
         return $this;
     }
-
+    /**
+     * Записывает схему
+     * @param string $scheme
+     * @return \Neiron\Kernel\Http\Uri
+     */
     public function withScheme($scheme) {
-        $this->parseUri['scheme'] = (string)$sheme;
+        $this->parseUri['scheme'] = (string)$scheme;
         return $this;
     }
-
+    /**
+     * Записывает логи и пароль авторизации
+     * @param string $user Логин
+     * @param string $password Пароль
+     * @return \Neiron\Kernel\Http\Uri
+     */
     public function withUserInfo($user, $password = null) {
         $this->parseUri['user'] = (string)$user;
         $this->parseUri['pass'] = (string)$password;
