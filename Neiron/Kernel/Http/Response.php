dae1b9f0da5cc7ff70701c5093683d2313d5682b
KpuTuK <KpuTuK@CodeMania-Team>
2015-10-15 08:33:20 +0300

    [ADD] Добавлена реализация ResponseInterface согласно psr-7

---
+++ Neiron/Kernel/Http/Response.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Neiron\Kernel\Http;
+
+/**
+ * Description of Response
+ *
+ * @author KpuTuK
+ */
+class Response {
+    //put your code here
+}
