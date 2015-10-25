ced2b45c12ff261a105831444dc306a9fc838256
KpuTuK <KpuTuK@CodeMania-Team>
2015-10-25 17:26:48 +0300

    Изменен Request класс

--- Neiron/Kernel/Http/Request.php
+++ Neiron/Kernel/Http/Request.php
@@ -21,9 +21,35 @@ class Request extends ServerRequest implements RequestInterface {
      * @var \Neiron\Kernel\DependencyInjection\DependencyInjectionInterface
      */
     protected $container;
-    /**
-     * URI
-     * @var \Psr\Http\Message\UriInterface
-     */
+    public function __construct($uri, $method) {
+        parent::__construct($uri);
+        $this->withMethod($method);
+    }
+    public function getMethod() {
+        
+    }
+
+    public function getRequestTarget() {
+        
+    }
+
+    public function getUri() {
+        return (string) $this->uri->getScheme() . $this->uri->getAuthority() .
+        $this->uri->getHost() . $this->uri->getPort() . $this->uri->getPath() .
+        $this->uri->getQuery() . $this->uri->getFragment();
+    }
+
+    public function withMethod($method) {
+        $this->server['HTTP_METHOD'] = $method;
+    }
+
+    public function withRequestTarget($requestTarget) {
+        
+    }
+
+    public function withUri(\Psr\Http\Message\UriInterface $uri, $preserveHost = false) {
+        
+    }
+
     protected $uri;
 }
