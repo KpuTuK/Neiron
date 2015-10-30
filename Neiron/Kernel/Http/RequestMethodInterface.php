30a3874bfa5d2baa35c911c9f9d3ad44d361bbe8
KpuTuK <KpuTuK@CodeMania-Team>
2015-10-30 17:27:00 +0300

    Добавлен интерфейс определяющий константы метода запроса

---
+++ Neiron/Kernel/Http/RequestMethodInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * PHP 5.4 framework с открытым иходным кодом
+ */
+
+namespace Neiron\Kernel\Http;
+
+/**
+ * Интерфейс определяющй константы метода запроса
+ * @author KpuTuK
+ * @version 1.0.0
+ * @package Neiron framework
+ * @category Http
+ */
+interface RequestMethodInterface {
+    const METH_HEAD = 'HEAD';
+    const METH_GET = 'GET';
+    const METH_POST = 'POST';
+    const METH_PUT = 'PUT';
+    const METH_PATCH = 'PATCH';
+    const METH_DELETE = 'DELETE';
+    const METH_PURGE = 'PURGE';
+    const METH_OPTIONS = 'OPTIONS';
+    const METH_TRACE = 'TRACE';
+    const METH_CONNECT = 'CONNECT';
+}
